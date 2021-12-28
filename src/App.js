1cddb72b247a4f9165e3b997ae1d9b4252666388
ymcla <ymclapp@yahoo.com>
2021-12-28 14:23:30 -0600

    moved update and delete functions to a component.  App works still

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
+import Todo from './components/Todo';
 
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div 
-    className="todo"
-    style={{ textDecoration:  todo.isCompleted ? "line-through" : "" }}
-    >
-      {todo.text}
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>Delete</button>
-      </div>
-    </div>
-  );
-};
 
 function TodoForm({ addTodo }) {
   const [value, setValue] = useState("");
