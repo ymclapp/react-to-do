03a89318530faae9b4dbc1734571501f1ed0815b
ymcla <ymclapp@yahoo.com>
2021-12-28 14:26:24 -0600

    moved add todo to its own file.  App is still working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Todo from './components/Todo';
-
-
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        placeholder="Add a New Item"
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = React.useState([
---
+++ src/components/TodoForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default function TodoForm({ addTodo }) {
+    const [value, setValue] = useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add a New Item"
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    );
+  }
