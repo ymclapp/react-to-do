5aaf7e646dee9647b4068d077fe84f661151ea56
ymcla <ymclapp@yahoo.com>
2021-12-28 12:58:48 -0600

    updated readme

---
+++ ProjectREADME.md
@@ -0,0 +1,5 @@
+###  Referenced:
+1.  https://www.digitalocean.com/community/tutorials/how-to-build-a-react-to-do-app-with-react-hooks
+
+###  Dependencies:
+1.  React
--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Todo ({ todo }) {
+function Todo({ todo }) {
   return (
     <div className="todo">
-      { todo.text }
+      {todo.text}
     </div>
   );
 };
 
 function App() {
-  const [todos, setTodos] = React.useState([  
+  const [todos, setTodos] = React.useState([
     //The first parameter, todos, is what you are going to name your state
     //The second parameter, setTodos, is what you are going to use to set the state
-    { text:  "Learn about React" },
-    { text:  "Meet friend for lunch" },
-    { text:  "Build really cool todo app" }
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
   ]);
 
   return (
     <div className="app">
       <div className="todo-list">
-        { todos.map((todo, index) => (
+        {todos.map((todo, index) => (
           <Todo
-          key={ index }
-          index={ index }
-          todo={ todo }
+            key={index}
+            index={index}
+            todo={todo}
           />
         ))}
       </div>
